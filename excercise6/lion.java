ab59de84b3bf3dfdbd70e557657f6a85d183e435
Quirf Ivan Onag <120693742+kindaterror@users.noreply.github.com>
2023-02-10 11:44:08 +0800

    Create lion.java

---
+++ excercise6/lion.java
@@ -0,0 +1,10 @@
+public class Lion implements Animal {
+   
+    public void walk(){
+        System.out.println("lion is walking ");
+    }
+    
+    public void run (){
+        System.out.println("lion is speed running like break  a leg ");
+    }
+}
