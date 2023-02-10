4b05afe01163749f7878992348ce17422062a2a2
Quirf Ivan Onag <120693742+kindaterror@users.noreply.github.com>
2023-02-10 11:45:12 +0800

    Create GroceryItem.java

---
+++ excercise6/GroceryItem.java
@@ -0,0 +1,10 @@
+public class Groceryitem {
+     String name;
+double price;
+     public void showItemName() {
+        System.out.println(this.name);
+     }
+     public void showprice() {
+        System.out.println(this.price);
+    }
+}
