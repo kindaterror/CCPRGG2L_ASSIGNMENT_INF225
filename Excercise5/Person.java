d32678261f9305a0911fe1cbd4c87b7e5da286ef
Quirf Ivan Onag <120693742+kindaterror@users.noreply.github.com>
2023-02-10 10:07:41 +0800

    Create Person.java

---
+++ Excercise5/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+
+     
+
+    private String name;
+    private int  ccnumber; 
+
+
+    public String getName() {
+        return this.name; 
+        
+    }
+ 
+    public void setName(String newName) {
+
+        this.name = newName;
+    }
+    
+    public int getnum() {
+        return this.ccnumber; 
+        
+    } 
+    
+    public void setnum(int ccnumber) {
+
+        this.ccnumber = ccnumber;
+    }
+
+}
